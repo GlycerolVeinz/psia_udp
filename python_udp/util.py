a602bb4c1c646828a6115b075451b52aada744a4
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-04-26 15:07:21 +0200

    finished implementing send and recieve functions

--- python_udp/util.py
+++ python_udp/util.py
@@ -1,18 +1,37 @@
 import const
 import socket
 import binascii
+from time import sleep
 
 
 def add_crc(package):
-    crc = binascii.crc32(package)
+    crc = binascii.crc32(package[const.CRC_SIZE : const.PACKAGE_SIZE])
     package[const.CRC_POS] = crc.to_bytes(const.CRC_SIZE, byteorder="big")
 
+def add_id(package, id):
+    if not hasattr(add_id, "id"):
+        add_id.id = 0
+    else:
+        add_id.id += 1
+    
+    return add_id.id.to_bytes(const.ID_SIZE, byteorder="big")
+    
 def create_packege(type, position, data):
     package = list(range(const.PACKAGE_SIZE))
     package[const.TYPE_POS] = type
     package[const.POSITION_POS] = position
     package[const.DATA_POS] = data
+    package[const.ID_POS] = add_id(package, add_id.id)
+    
     return package
+
+def recieve_package(size, sock : socket.socket):
+    package = sock.recv(size)
+    crc = binascii.crc32(package[const.CRC_POS : const.PACKAGE_SIZE])
+    if crc == int.from_bytes(package[const.CRC_POS], byteorder="big"):
+        return package
+    else:
+        return None
 
 def send_package(sock : socket.socket, package, target_address : tuple):
     # add crc
@@ -21,17 +40,21 @@ def send_package(sock : socket.socket, package, target_address : tuple):
     sock.sendto(package, target_address)
     
     # recieve acknowledge from receiver
-    r_packet, r_address = sock.recvfrom(const.PACKAGE_SIZE)
-    
-    while r_packet != const.ACKNOWLEDGE_MARKER:
-        if r_packet == const.DENIED_MARKER:
-            # send again if failed
+    r_packet = recieve_package(const.PACKAGE_SIZE, sock)
+    while True:
+        if r_packet == None:
+            # send package again (crc is wrong)
             sock.sendto(package, target_address)
-        elif r_packet == const.SENDER_ERROR_MARKER:
-            print("Error: sender had an error")
-            sock.close()
-            exit()
+        elif r_packet[const.TYPE_POS] != const.MARKER_TYPE:
+            # send package again (package type is wrong)
+            sock.sendto(package, target_address)
+        elif r_packet[const.DATA_POS] == const.ACKNOWLEDGE_MARKER:
+            # acknowledge is correct
+            break
         else:
-            raise Exception("Error: unknown error")
-        r_packet, r_address = sock.recvfrom(const.PACKAGE_SIZE)
+            # send package again (unknown error)
+            sock.sendto(package, target_address)
+        
+        sleep(0.0001)
+        r_packet = recieve_package(const.PACKAGE_SIZE, sock)
     
