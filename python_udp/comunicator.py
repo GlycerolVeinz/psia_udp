7959681ad2d27785b781b3b52e75c3c51f001124
GlycerolVeinz <safrankou.matvei@gmail.com>
2023-05-07 22:13:44 +0200

    Finished task 3, need to test it in class

--- python_udp/comunicator.py
+++ python_udp/comunicator.py
@@ -34,19 +34,24 @@ if side == "S":
         # read and send data
         last = False
         reading = True
+        tries = 0
+        end_package = None
         while True:
             # read data
             if len(packages) != c.SENDING_WINDOW and reading:
+                tries = 0
                 position = int(o_file.tell())
                 package = u.create_packege(c.DATA_TYPE, position, o_file.read(c.DATA_SIZE))
                 hash_num.update(package[c.DATA_POS])
 
-                if (packages[-1][c.DATA_POS] == b"") or (len(packages[-1]) != c.PACKAGE_SIZE):
+                
+                if (package[c.DATA_POS] == b""):
                     end_package = u.create_packege(c.MARKER_TYPE, None, c.END_MARKER)
                     packages.append(end_package)
                     reading = False
                 else:
                     packages.append(package)
+                
             
             # send data
             for pack in packages:
@@ -54,16 +59,22 @@ if side == "S":
             
                 # get acknowledge
                 try:
-                    r_package = u.recieve_package_ack(c.PACKAGE_SIZE, sock, 0.1)
+                    r_package = u.recieve_package(c.PACKAGE_SIZE, sock, 0.1)
                 except TimeoutError:
                     r_package = None
                     
-                if (r_package[c.TYPE_POS] == c.MARKER_TYPE) and (r_package[c.DATA_POS] == c.ACKNOWLEDGE_MARKER) and (r_package[c.ID_POS] == pack[c.ID_POS]):
-                    if pack[c.ID_POS] == end_package[c.ID_POS]:
+                if r_package and (r_package[c.TYPE_POS] == c.MARKER_TYPE) and (r_package[c.DATA_POS] == c.ACKNOWLEDGE_MARKER) and (r_package[c.ID_POS] in [x[c.ID_POS] for x in packages]):
+                    if end_package and (r_package[c.ID_POS] == end_package[c.ID_POS]):
                         last = True
                     packages = [x for x in packages if x[c.ID_POS] != r_package[c.ID_POS]]
                 
-                                     
+            
+            tries += 1
+            if tries == c.MAX_TRIES:
+                # TODO send error message
+                sock.close()
+                raise Exception(c.ERROR_MAX_TRIES)
+                                   
             if last and len(packages) == 0:
                 break
 
@@ -92,53 +103,53 @@ elif side == "R":
             raise Exception(c.ERROR_START_MARKER)
 
         # Write data
-        packages = list()
         last = False
-        recieving = True
         while True:
-            # recieve package
-            if recieving:
-                try:
-                    package = u.recieve_package(c.PACKAGE_SIZE, sock, 0.1)
-                except TimeoutError:
-                    package = None
-
-                packages.append(package)
-
-            # check validity of package
-            for pack in packages:
-                if (pack is None):
-                    packages.remove(pack)
+            # recieve package 
+            tries = 0
+            try:
+                package = u.recieve_package(c.PACKAGE_SIZE, sock, 0.1)
+                if (package is None):
                     s_package = u.create_packege(c.MARKER_TYPE, None, c.DENIED_MARKER)
                     sock.sendto(s_package, c.SENDER_ADRESS)
-                    continue
                 
-                elif (pack[c.TYPE_POS] == c.MARKER_TYPE) and (pack[c.DATA_POS] == c.SENDER_ERROR_MARKER):
+                elif (package[c.TYPE_POS] == c.MARKER_TYPE) and (package[c.DATA_POS] == c.SENDER_ERROR_MARKER):
                     sock.close()
                     exit(c.ERROR_SENDER_ERROR)
                 
-                elif (pack[c.TYPE_POS] == c.MARKER_TYPE) and (pack[c.DATA_POS] == c.END_MARKER):
-                    recieving = False
-                    packages.remove(pack)
+                elif (package[c.TYPE_POS] == c.MARKER_TYPE) and (package[c.DATA_POS] == c.END_MARKER):
+                    last = True
                 
-                elif (pack[c.TYPE_POS] != c.DATA_TYPE):
-                    packages.remove(pack)
-                    continue
-                    
-                # send acknowledge
-                s_package = u.create_packege(c.MARKER_TYPE, None, c.ACKNOWLEDGE_MARKER, id = pack[c.ID_POS])    
-                sock.sendto(s_package, c.SENDER_ADRESS)    
-                
-                # write data to file
-                recieved_f.seek(int.from_bytes(pack[c.POSITION_POS], byteorder="big"))
-                recieved_f.write(pack[c.DATA_POS])
-                hash_num.update(pack[c.DATA_POS])
-                packages.remove(pack) 
+                elif (package[c.TYPE_POS] != c.DATA_TYPE):
+                    s_package = u.create_packege(c.MARKER_TYPE, None, c.DENIED_MARKER)
+                    sock.sendto(s_package, c.SENDER_ADRESS)
+                                    
+            except TimeoutError:
+                tries += 1
 
+
+            
+            # send acknowledge
+            s_package = u.create_packege(c.MARKER_TYPE, None, c.ACKNOWLEDGE_MARKER, id = package[c.ID_POS])    
+            sock.sendto(s_package, c.SENDER_ADRESS)    
+            
+            # write data to file
+            if package[c.DATA_POS] != c.END_MARKER:
+                recieved_f.seek(int.from_bytes(package[c.POSITION_POS], byteorder="big"))
+                recieved_f.write(package[c.DATA_POS])
+                    
+                    
+            if tries == c.MAX_TRIES:
+                sock.close()
+                raise Exception(c.ERROR_MAX_TRIES)
+            
             if last:    
                 break
         
-    # Recieve and compare hash
+    # Compare hash
+    with open("output_" + file_name, "rb") as tmp:
+        hash_num.update(tmp.read())
+        
     hash_num = hash_num.digest()
     package = u.recieve_package_ack(c.PACKAGE_SIZE, sock)
     if package[c.DATA_POS] != hash_num:
